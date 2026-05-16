81e027442731d14cdc4b4d1ec3112224fc82d0c8
ysaac12 <ysaac06mesa@gmail.com>
2026-05-16 13:42:01 +0800

    task 3 done

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,44 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+
+    cart.forEach(item => {
+      const costValue = parseFloat(item.cost.replace('$', ''))
+      total += costValue * item.quantity
+    })
+    return total
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping();
   };
 
+  const handleCheckout = (e) => {
+    alert('Working in Progress')
+  }
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity+1}))
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.quantity>1) {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity-1}))
+   } else {
+    dispatch(removeItem(item.name))
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const costValue =  parseFloat(item.cost.replace($, ''))
+    return costValue * item.quantity
   };
 
   return (
